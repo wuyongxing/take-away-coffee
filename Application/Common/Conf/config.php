f47d8a688b3bab5fb75011e315c0306d0646a010
whatyouthink <350193001@qq.com>
2016-06-01 22:52:04 +0800

    begin

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_DSN'=>'mysql://root:123456@localhost:3306/tacoffee',//使用DSN方式配置数据库信息
+	'SHOW_PAGE_TRACE'=>true,//开启页面Trace
+	'OUTPUT_ENCODE' =>  false,
+);
---
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -0,0 +1,9 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<title>Coffee Management</title>
+</head>
+<body>
+	
+</body>
+</html>
